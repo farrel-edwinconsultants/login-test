c329348a97de0f71a38bb21797099fa93765ec4e
farrel-edwinconsultants <105842139+farrel-edwinconsultants@users.noreply.github.com>
2022-05-30 11:00:07 +0700

    first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { getUser, removeUserSession } from '../utils/Common';
+
+function Dashboard(props) {
+    const user = getUser();
+
+    const handleLogout = () => {
+        removeUserSession();
+        props.history.push('/login');
+    }
+  return (
+    <div>
+      Welcome User!<br /><br />
+      <input type="button" onClick={handleLogout} value="Logout" />
+    </div>
+  )
+}
+
+export default Dashboard
